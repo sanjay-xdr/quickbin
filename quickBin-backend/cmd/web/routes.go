45fd21c1548288c2cf3e427c91f3ed3a8c2fe8f6
Sanjay Jaiswal <sanjay.jaiswal@cginfinity.com>
2025-06-04 10:59:58 +0530

    adding cors

--- quickBin-backend/cmd/web/routes.go
+++ quickBin-backend/cmd/web/routes.go
@@ -6,6 +6,7 @@ import (
 	"quickbin/internals/repository"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
 )
 
 func (app *Config) Routes() http.Handler {
@@ -13,6 +14,15 @@ func (app *Config) Routes() http.Handler {
 	mux := chi.NewRouter()
 	repo := repository.NewSnippetRepository(app.DB)
 	snippet := handlers.NewSnippetHandler(repo) // pass the repo
+
+	mux.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
 	mux.Get("/", func(w http.ResponseWriter, r *http.Request) {
 
 		w.Write([]byte("Hello World"))
--- quickBin-backend/go.mod
+++ quickBin-backend/go.mod
@@ -2,8 +2,10 @@ module quickbin
 
 go 1.24.1
 
+require github.com/lib/pq v1.10.9
+
 require (
 	github.com/go-chi/chi/v5 v5.2.1 // indirect
+	github.com/go-chi/cors v1.2.1 // indirect
 	github.com/google/uuid v1.6.0 // indirect
-	github.com/lib/pq v1.10.9
 )
